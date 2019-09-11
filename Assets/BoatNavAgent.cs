3e6b523253815e86a689f78d73a9866ade28a23e
baaleze <baaleze@gmail.com>
2019-09-11 17:17:42 +0200

    Some movement with a boat is possible

---
+++ Assets/BoatNavAgent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BoatNavAgent : MonoBehaviour
+{
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000)) {
+                Debug.Log(hit);
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
